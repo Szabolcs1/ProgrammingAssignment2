ff6e061b54ebde4bd5de46c238348393038cf59c
Szabolcs Farkas <sznfarkas@gmail.com>
2014-08-24 14:26:28 -0700

    This is final version of the program.

--- cachematrix.R
+++ cachematrix.R
@@ -1,30 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## This program will create and cache the inverse of a square matrix.
+
+##This first function, makeCacheMatrix, creates a special "vector".
+##This special "vector" is a list containing a function to:
+## - set a value of the vector
+## - get the value of the vector
+## - set the value of the inverse of the matrix
+## - get the value of the inverse of the matrix
+
 
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
+        inv <- NULL
         set <- function(y) {
                 x <<- y
                 inv <<- NULL
         }
         get <- function() x
-        setinv <- function(mean) inv <<- mean
-        getinv <- function() m
+        setinv <- function(inverse) inv <<- inverse
+        getinv <- function() inv
         list(set = set, get = get,
              setinv = setinv,
              getinv = getinv)
 }
 
-
-
-
-## Write a short comment describing this function
+## This function, cacheSolve, calculates the inverse 
+## of the special "vector" created by function makeCacheMatrix. 
+## If first checks to see whether the inverse of the matrix has already been calculated. 
+## If so, it gets the inverse of the matrix from the cache and skips 
+## the computation and gives the inverse of 
+## the matrix with the comments "getting cached data". 
+## Otherwise, it will calculate the inverse of the matrix and 
+## sets the value of the inverse of the
+## matrix in the cache via setinv function.
 
 cacheSolve <- function(x, ...) {
                 inv <- x$getinv()
-                if(!is.null(m)) {
+                if(!is.null(inv)) {
                         message("getting cached data")
                         return(inv)
                 }
@@ -33,5 +44,5 @@ cacheSolve <- function(x, ...) {
                 x$setinv(inv)
                 inv
 }
-        ## Return a matrix that is the inverse of 'x'
+        
 
